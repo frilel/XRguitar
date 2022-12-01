448eee0eec1e3c22cd4aa1bbf2fa446f3cc301ce
frilel <crillefrille@gmail.com>
2022-12-01 18:41:39 +0100

    update scene
    add bbending UI and float calculation

--- Assets/GuitarManager.cs
+++ Assets/GuitarManager.cs
@@ -1,18 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GuitarManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform OVRControllerVisual;
+    public Transform MinBendingTransform;
+    public Transform MaxBendingTransform;
+    public LineRenderer BbendingLineRenderer;
+    public TMPro.TMP_Text BendingInfoText;
+
+    public float BBendingValue { get; private set; } = 0f;
+
+    private Vector3 MinBendingPos;
+    private Vector3 MaxBendingPos;
+
+    private bool HasBendingRange = false;
+    private int[] bendingSet = new int[2];
+
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateBendingTransforms();
+
+        CheckBendingSet();
+
+        if (HasBendingRange)
+        {
+            if (OVRControllerVisual.position.y >= MinBendingTransform.position.y)
+                BBendingValue = 0f;
+            else if (OVRControllerVisual.position.y <= MinBendingTransform.position.y)
+                BBendingValue = 1f;
+            else
+            {
+                float maxDist = Vector3.Distance(MinBendingTransform.position, MaxBendingTransform.position);
+                float currDist = Vector3.Distance(MinBendingTransform.position, OVRControllerVisual.position);
+
+                if (currDist == 0f)
+                    BBendingValue = 0f;
+                else
+                    BBendingValue = currDist / maxDist;
+
+
+            }
+
+            BbendingLineRenderer.SetPosition(0, MinBendingTransform.position);
+            BbendingLineRenderer.SetPosition(1, MaxBendingTransform.position);
+
+            BendingInfoText.text = $"Bending value: {BBendingValue}";
+        }
     }
+
+    private void UpdateBendingTransforms()
+    {
+        MinBendingTransform.position = new Vector3(OVRControllerVisual.position.x, MinBendingPos.y, OVRControllerVisual.position.z);
+        MaxBendingTransform.position = new Vector3(OVRControllerVisual.position.x, MaxBendingPos.y, OVRControllerVisual.position.z);
+    }
+
+    private void CheckBendingSet()
+    {
+        if (bendingSet[0] == 1 && bendingSet[1] == 1)
+            HasBendingRange = true;
+    }
+
+    public void OnMaxBendingReset()
+    {
+        MaxBendingPos = OVRControllerVisual.position;
+
+        bendingSet[1] = 1;
+    }
+
+    public void OnMinBendingReset()
+    {
+        MinBendingPos = OVRControllerVisual.position;
+
+        bendingSet[0] = 1;
+    }
+
 }
