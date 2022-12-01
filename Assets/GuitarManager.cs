fd0b182efef1036bfbff97235dbeeec7e5debca6
frilel <crillefrille@gmail.com>
2022-12-01 19:01:01 +0100

    update bbending calculation

--- Assets/GuitarManager.cs
+++ Assets/GuitarManager.cs
@@ -4,52 +4,45 @@ using UnityEngine.UI;
 
 public class GuitarManager : MonoBehaviour
 {
-    public Transform OVRControllerVisual;
-    public Transform MinBendingTransform;
-    public Transform MaxBendingTransform;
+    public Transform LeftHandAnchor;
+    public Transform TopBendingTransform;
+    public Transform BottomBendingTransform;
     public LineRenderer BbendingLineRenderer;
     public TMPro.TMP_Text BendingInfoText;
 
     public float BBendingValue { get; private set; } = 0f;
 
-    private Vector3 MinBendingPos;
-    private Vector3 MaxBendingPos;
+    private Vector3 SavedTopBendingPos;
+    private Vector3 SavedBottomBendingPos;
 
-    private bool HasBendingRange = false;
-    private int[] bendingSet = new int[2];
-
-    private void Start()
-    {
-
-    }
+    private bool HasBendingSet = false;
+    private bool[] bendingSet = { false, false };
 
     private void Update()
     {
         UpdateBendingTransforms();
 
-        CheckBendingSet();
+        CheckBendingIsSet();
 
-        if (HasBendingRange)
+        if (HasBendingSet)
         {
-            if (OVRControllerVisual.position.y >= MinBendingTransform.position.y)
+            if (LeftHandAnchor.position.y >= TopBendingTransform.position.y)
                 BBendingValue = 0f;
-            else if (OVRControllerVisual.position.y <= MinBendingTransform.position.y)
+            else if (LeftHandAnchor.position.y <= BottomBendingTransform.position.y)
                 BBendingValue = 1f;
             else
             {
-                float maxDist = Vector3.Distance(MinBendingTransform.position, MaxBendingTransform.position);
-                float currDist = Vector3.Distance(MinBendingTransform.position, OVRControllerVisual.position);
+                float maxDist = Vector3.Distance(TopBendingTransform.position, BottomBendingTransform.position);
+                float currDist = Vector3.Distance(TopBendingTransform.position, LeftHandAnchor.position);
 
                 if (currDist == 0f)
                     BBendingValue = 0f;
                 else
                     BBendingValue = currDist / maxDist;
-
-
             }
 
-            BbendingLineRenderer.SetPosition(0, MinBendingTransform.position);
-            BbendingLineRenderer.SetPosition(1, MaxBendingTransform.position);
+            BbendingLineRenderer.SetPosition((int)BendingSet.Top, TopBendingTransform.position);
+            BbendingLineRenderer.SetPosition((int)BendingSet.Bottom, BottomBendingTransform.position);
 
             BendingInfoText.text = $"Bending value: {BBendingValue}";
         }
@@ -57,28 +50,32 @@ public class GuitarManager : MonoBehaviour
 
     private void UpdateBendingTransforms()
     {
-        MinBendingTransform.position = new Vector3(OVRControllerVisual.position.x, MinBendingPos.y, OVRControllerVisual.position.z);
-        MaxBendingTransform.position = new Vector3(OVRControllerVisual.position.x, MaxBendingPos.y, OVRControllerVisual.position.z);
+        TopBendingTransform.position = new Vector3(LeftHandAnchor.position.x, SavedTopBendingPos.y, LeftHandAnchor.position.z);
+        BottomBendingTransform.position = new Vector3(LeftHandAnchor.position.x, SavedBottomBendingPos.y, LeftHandAnchor.position.z);
     }
 
-    private void CheckBendingSet()
+    private void CheckBendingIsSet()
     {
-        if (bendingSet[0] == 1 && bendingSet[1] == 1)
-            HasBendingRange = true;
+        if (bendingSet[(int)BendingSet.Top] && bendingSet[(int)BendingSet.Bottom])
+            HasBendingSet = true;
     }
 
     public void OnMaxBendingReset()
     {
-        MaxBendingPos = OVRControllerVisual.position;
+        SavedBottomBendingPos = LeftHandAnchor.position;
 
-        bendingSet[1] = 1;
+        bendingSet[(int)BendingSet.Bottom] = true;
     }
 
     public void OnMinBendingReset()
     {
-        MinBendingPos = OVRControllerVisual.position;
+        SavedTopBendingPos = LeftHandAnchor.position;
 
-        bendingSet[0] = 1;
+        bendingSet[(int)BendingSet.Top] = true;
     }
-
+    private enum BendingSet
+    {
+        Top,
+        Bottom
+    }
 }
